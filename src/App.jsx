3115dec80b674f03376f6f59478dc4d27cec3df8
unknown <milagos31@gmail.com>
2023-06-21 20:03:18 +0800

    modify details state to persist when turning back

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import Form from "./form";
 import Display from "./display";
 
 function App() {
-    const [details, setDetails] = useState({});
+    const [details, setDetails] = useState({
+        name: "",
+        age: "",
+        contactNumber: "",
+        favoriteFood: "",
+        favoriteColor: "",
+    });
     const [isSubmitted, setSubmit] = useState(false);
     return (
         <>
             {isSubmitted ? (
                 <Display details={details} setSubmit={setSubmit} />
             ) : (
-                <Form setDetails={setDetails} setSubmit={setSubmit} />
+                <Form setDetails={setDetails} setSubmit={setSubmit} details={details} />
             )}
         </>
     );
--- src/form/index.jsx
+++ src/form/index.jsx
@@ -1,9 +1,10 @@
 import "./style.css";
-export default function Form({ setDetails, setSubmit }) {
+export default function Form({ setDetails, setSubmit, details }) {
+    const { name, age, contactNumber, favoriteFood, favoriteColor } = details;
     const formChange = (e) => {
         const property = e.target.id;
         const value = e.target.value;
-        setDetails((prev) => ({ ...prev, [property]: value }));
+        setDetails((details) => ({ ...details, [property]: value }));
     };
 
     const submit = (e) => {
@@ -13,19 +14,19 @@ export default function Form({ setDetails, setSubmit }) {
     return (
         <form onChange={formChange} onSubmit={submit}>
             <label>
-                Name: <input id="name" />
+                Name: <input id="name" value={name} />
             </label>
             <label>
-                Age: <input id="age" />
+                Age: <input id="age" value={age} />
             </label>
             <label>
-                Contact Number: <input id="contactNumber" />
+                Contact Number: <input id="contactNumber" value={contactNumber} />
             </label>
             <label>
-                Favorite color: <input id="favoriteColor" />
+                Favorite color: <input id="favoriteColor" value={favoriteColor} />
             </label>
             <label>
-                Favorite food: <input id="favoriteFood" />
+                Favorite food: <input id="favoriteFood" value={favoriteFood} />
             </label>
             <button type="submit">Submit</button>
         </form>
